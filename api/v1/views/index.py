95350e5b436eccd82d7782d97c2b014238a8da50
deking254 <dekingsky@gmail.com>
2023-09-30 20:38:37 +0000

    made the index.py file pep8 compliant

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,11 @@ from models import storage
 from models.state import State
 from models.amenity import Amenity
 from models.city import City
-from models.place  import Place
-from models.review  import Review
+from models.place import Place
+from models.review import Review
 from models.user import User
+
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns the status of an API"""
@@ -16,23 +18,24 @@ def status():
         }
     return j
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """returns number of each class"""
     stats = {
-              "amenities": 0, 
-              "cities": 0, 
-              "places": 0, 
-              "reviews": 0, 
-              "states": 0, 
+              "amenities": 0,
+              "cities": 0,
+              "places": 0,
+              "reviews": 0,
+              "states": 0,
               "users": 0
             }
-    amenities =  storage.count(Amenity)
-    cities =  storage.count(City)
-    places =  storage.count(Place)
-    reviews =  storage.count(Review)
-    states =  storage.count(State)
-    users =  storage.count(User)
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
     stats.update({
               "amenities": amenities,
               "cities": cities,
