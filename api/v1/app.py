e6ff5d46051e8f4c72dcd2d8ce22f21e2490e24e
Kiirya Arnold <kiiryarnold@gmail.com>
2023-10-02 17:44:27 +0300

    corrected

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""a functon to get status of API"""
+"""a function to get the status of the API"""
 import os
 from models import storage
-from flask.app import Flask
-from flask import make_response, jsonify
+from flask import Flask, jsonify
+from flask_cors import CORS  # Import CORS from flask_cors
 from api.v1.views import app_views
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
+# Configure CORS to allow requests from all origins for all API routes
+CORS(app)
 
 @app.teardown_appcontext
 def handle_teardown(exception):
     """handle @app.teardown_appcontext"""
     storage.close()
 
-
 @app.errorhandler(404)
 def not_found(exception):
     data = {"error": "Not found"}
     return jsonify(data), 404
-
 
 if __name__ == "__main__":
     hst = os.getenv('HBNB_API_HOST')
